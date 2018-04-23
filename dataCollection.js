0b0e5bd25fc462f10a64fbbd1f70e07c28508367
prateekkuk <prateek.kukreja@gmail.com>
2018-04-23 17:25:20 -0400

    changed scraper to get the percent values

--- dataCollection.js
+++ dataCollection.js
@@ -12,7 +12,10 @@ var scrapedData = [];
 dataCollection.urls = [];
 dataCollection.urls.push({name:"Metformin",url:'https://www.patientslikeme.com/treatments/show/221'}); 
 dataCollection.urls.push({name:"Insulin Glargine", url: "https://www.patientslikeme.com/treatments/show/8306"});
+dataCollection.urls.push({name:"Advil", url: "https://www.patientslikeme.com/treatments/show/319"});
 
+//TODO maybe use promises and dont need to store the HTML files in a folder, do a .then and add to 
+//the data collection object
 
 dataCollection.parsingUrls = function(){
     //create html page for each url
@@ -44,44 +47,56 @@ var getData = function(drugFile, drugName){
     
     var $ = cheerio.load(drugFile);
     var section = [];
-    
-    $('div[id="overview"]').find('article > div > div').each(function (index, element) {
-        section.push($(element).text());
-    });
-    var sideEffectObj = {};
-    var cleanSideEffectList = []; 
-    var patientNumber;
-    var sideEffectString = section[4].toString();
-    var sideEffectlist = sideEffectString.split(":");
-    sideEffectlist.forEach((curr,index,all) =>{
-        //curr.replace(//g,"");
-        if(index > 4){
-            cleanSideEffectList.push(curr.slice(0,-28));
+    var sideEffectColumnsHTML = [];
+    var drugSideEffectsColumnHTML;
+    var sideEffectName = [];
+    var sideEffectPercents = [];
+
+    // $('div[id="overview"]').find('article > div > div > div').each(function (index, element) {
+    //     section.push($(element).html());
+    // });
+    $('div').each((index,element) => {
+        if($(element).hasClass('content-section')){
+            section.push($(element).html());
         }
     });
-    cleanSideEffectList.forEach((curr,index,all) => {
-        var patientNumberRepeated = curr.match(/\d+/)[0];
-        patientNumber = patientNumberRepeated.slice(0,(patientNumberRepeated.length/2));
-        var numberIndex = curr.indexOf(patientNumberRepeated);
-        cleanSideEffectList[index] = curr.slice(0,numberIndex);
-        sideEffectObj[cleanSideEffectList[index]] = patientNumber;
-    })
-    //console.log(cleanSideEffectList);
-    var showIndex = cleanSideEffectList[cleanSideEffectList.length-1].indexOf("Show");
-    cleanSideEffectList[cleanSideEffectList.length-1] = cleanSideEffectList[cleanSideEffectList.length-1].slice(0,showIndex)
-    
-    //scrapedData.push([drugName]); //= sideEffectObj;
-    var objTmp = {};
-    objTmp[drugName] = sideEffectObj;
-    console.log(objTmp);
-    scrapedData.push(objTmp);
-    writeDataToFile();
-};
+    var sideEffectHTML = section[1];
 
+    $ = cheerio.load(sideEffectHTML);
+    $('div').each((index, element)=> {
+        if($(element).hasClass('columns')){
+            sideEffectColumnsHTML.push($(element).html());
+        }
+    })    
+    drugSideEffectsColumnHTML = sideEffectColumnsHTML[1];
+    $ = cheerio.load(drugSideEffectsColumnHTML);
+    $('span').each((index,element) => {
+        if($(element).attr('itemprop') == "name"){
+            sideEffectName.push($(element).text());
+        }
+    });
+
+    $('li').each((index,element)=>{
+        if($(element).hasClass('bar-graph-item')){
+            var percentString = $(element).attr('style').split(":");
+            sideEffectPercents.push(parseFloat(percentString[1]));
+        }
+    })
+    
+ 
+    var objTmp = {};
+    for(var i = 0; i<sideEffectName.length;i++){
+        objTmp[sideEffectName[i]] = sideEffectPercents[i];
+    }
+    var tmpDrugObj = {}
+    tmpDrugObj[drugName] = objTmp;
+    scrapedData.push(tmpDrugObj);
+    writeDataToFile();
+
+};
 
 var writeDataToFile = function(){
     var scrapedDataJSON = JSON.stringify(scrapedData);
-    //var cleaned = scrapedDataJSON.Replace("\\", "");
     fs.writeFileSync("drugData.js",'var data ='+scrapedDataJSON,"utf8");
     showData(scrapedDataJSON);
 }
